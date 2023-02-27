4c6085a5745ee16995ebe9c8facfeefdeec63204
LukasVenancio <lukassvenancio022@gmail.com>
2023-02-27 13:26:05 -0300

    In progress: Close a filter when other is clicked

--- src/presentation/atomic-components/organisms/proposalsTable/proposalsTable.tsx
+++ src/presentation/atomic-components/organisms/proposalsTable/proposalsTable.tsx
@@ -8,7 +8,8 @@ import {
   TableRow
 } from '@mui/material';
 import { SimpleFilterTable } from 'presentation/atomic-components/molecules/simple-table-filter/simple-table-filter';
-import type { FC } from 'react';
+import { useState } from 'react';
+import type { FC, ReactElement } from 'react';
 import type { Filter } from 'presentation/atomic-components/molecules/simple-table-filter/simple-table-filter';
 
 interface unity {
@@ -40,133 +41,142 @@ interface ProposalsTableProps {
   userType: 'collaborator' | 'company';
 }
 
-export const ProposalsTable: FC<ProposalsTableProps> = ({ proposals, filterOptions, userType }) => (
-  <div>
-    <TableContainer className={'bg-white'}>
-      <Table aria-label={'simple table'} sx={{ minWidth: 650 }}>
-        <TableHead className={'font-semibold'}>
-          <TableRow>
-            <TableCell align={'justify'} variant={'head'}>
-              <div className={'flex justify-between'}>
-                <span>Etapa</span>
-                <SimpleFilterTable
-                  filterOptions={filterOptions}
-                  filterSide={'right'}
-                  title={'Etapa'}
-                />
-              </div>
-            </TableCell>
-            <TableCell align={'left'} variant={'head'}>
-              <div className={'flex justify-between'}>
-                <span>Propostas</span>
-                <SimpleFilterTable filterSide={'right'} title={'Propostas'} />
-              </div>
-            </TableCell>
-            <TableCell align={'left'} variant={'head'}>
-              <div className={'flex justify-between'}>
-                <span>Nome</span>
-                <SimpleFilterTable
-                  filterOptions={filterOptions}
-                  filterSide={'right'}
-                  title={'Nome'}
-                />
-              </div>
-            </TableCell>
-            <TableCell align={'left'} variant={'head'}>
-              <div className={'flex justify-between'}>
-                <span>Produto Tecnológico</span>
-                <SimpleFilterTable
-                  filterOptions={filterOptions}
-                  filterSide={'right'}
-                  title={'Produto Tecnológico'}
-                />
-              </div>
-            </TableCell>
-            <TableCell align={'left'} variant={'head'}>
-              <div className={'flex justify-between'}>
-                <span>CNPJ</span>
-                <SimpleFilterTable
-                  filterOptions={filterOptions}
-                  filterSide={'right'}
-                  title={'CNPJ'}
-                />
-              </div>
-            </TableCell>
-            {userType === 'collaborator' ? (
-              <TableCell align={'left'} variant={'head'}>
-                <div className={'flex justify-between w-full h-full'}>
-                  <span>Status</span>
-                  <SimpleFilterTable filterSide={'left'} title={'Status'} />
+export const ProposalsTable: FC<ProposalsTableProps> = ({ proposals, filterOptions, userType }) => {
+  const [openFilter, setOpenFilter] = useState<ReactElement | null>();
+  const open = Boolean(openFilter);
+
+  const hadleClickFilter = (event: React.MouseEvent<ReactElement>): void => {
+    setOpenFilter(event.currentTarget);
+  };
+
+  return (
+    <div>
+      <TableContainer className={'bg-white'}>
+        <Table aria-label={'simple table'} sx={{ minWidth: 650 }}>
+          <TableHead className={'font-semibold'}>
+            <TableRow>
+              <TableCell align={'justify'} variant={'head'}>
+                <div className={'flex justify-between'}>
+                  <span>Etapa</span>
+                  <SimpleFilterTable
+                    filterOptions={filterOptions}
+                    filterSide={'right'}
+                    title={'Etapa'}
+                  />
                 </div>
               </TableCell>
-            ) : null}
-            <TableCell align={'left'} variant={'head'}>
-              <div className={'flex justify-between items-center'}>
-                <span>Unid. Relac.</span>
-                <SimpleFilterTable
-                  filterOptions={filterOptions}
-                  filterSide={'left'}
-                  title={'Unid. Relac.'}
-                />
-              </div>
-            </TableCell>
-            <TableCell align={'left'} variant={'head'}>
-              <div className={'flex justify-between items-center'}>
-                <span>Unid. Exec.</span>
-                <SimpleFilterTable
-                  filterOptions={filterOptions}
-                  filterSide={'left'}
-                  title={'Unid. Exec.'}
-                />
-              </div>
-            </TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {proposals.map((item) => (
-            <TableRow key={item.id} className={'uppercase'}>
-              <TableCell align={'left'} component={'th'} scope={'row'}>
-                {item.step}
+              <TableCell align={'left'} variant={'head'}>
+                <div className={'flex justify-between'}>
+                  <span>Propostas</span>
+                  <SimpleFilterTable filterSide={'right'} title={'Propostas'} />
+                </div>
               </TableCell>
-              <TableCell align={'left'}>{item.proposal}</TableCell>
-              <TableCell align={'left'}>{item.name}</TableCell>
-              <TableCell align={'left'}>{item.techProduct}</TableCell>
-              <TableCell align={'left'}>{item.cnpj}</TableCell>
+              <TableCell align={'left'} variant={'head'}>
+                <div className={'flex justify-between'}>
+                  <span>Nome</span>
+                  <SimpleFilterTable
+                    filterOptions={filterOptions}
+                    filterSide={'right'}
+                    title={'Nome'}
+                  />
+                </div>
+              </TableCell>
+              <TableCell align={'left'} variant={'head'}>
+                <div className={'flex justify-between'}>
+                  <span>Produto Tecnológico</span>
+                  <SimpleFilterTable
+                    filterOptions={filterOptions}
+                    filterSide={'right'}
+                    title={'Produto Tecnológico'}
+                  />
+                </div>
+              </TableCell>
+              <TableCell align={'left'} variant={'head'}>
+                <div className={'flex justify-between'}>
+                  <span>CNPJ</span>
+                  <SimpleFilterTable
+                    filterOptions={filterOptions}
+                    filterSide={'right'}
+                    title={'CNPJ'}
+                  />
+                </div>
+              </TableCell>
               {userType === 'collaborator' ? (
-                <TableCell align={'left'}>
-                  <div className={'flex flex-col gap-2'}>
-                    <Chip
-                      label={'Reg. de conclusão'}
-                      size={'small'}
-                      variant={item.status.conclusionRecord === true ? 'filled' : 'outlined'}
-                    />
-                    <Chip
-                      label={'SGSET'}
-                      size={'small'}
-                      variant={item.status.sgset === true ? 'filled' : 'outlined'}
-                    />
-                    {item.status.diagnostic ? (
-                      <Chip
-                        label={'Diagnóstico'}
-                        size={'small'}
-                        variant={item.status.diagnostic === true ? 'filled' : 'outlined'}
-                      />
-                    ) : null}
+                <TableCell align={'left'} variant={'head'}>
+                  <div className={'flex justify-between w-full h-full'}>
+                    <span>Status</span>
+                    <SimpleFilterTable filterSide={'left'} title={'Status'} />
                   </div>
                 </TableCell>
               ) : null}
-              <TableCell align={'left'}>
-                {userType === 'collaborator'
-                  ? item.relationshipUnity.cfp
-                  : item.relationshipUnity.name}
+              <TableCell align={'left'} variant={'head'}>
+                <div className={'flex justify-between items-center'}>
+                  <span>Unid. Relac.</span>
+                  <SimpleFilterTable
+                    filterOptions={filterOptions}
+                    filterSide={'left'}
+                    title={'Unid. Relac.'}
+                  />
+                </div>
               </TableCell>
-              <TableCell align={'left'}>
-                {userType === 'collaborator' ? item.executingUnity.cfp : item.executingUnity.name}
+              <TableCell align={'left'} variant={'head'}>
+                <div className={'flex justify-between items-center'}>
+                  <span>Unid. Exec.</span>
+                  <SimpleFilterTable
+                    filterOptions={filterOptions}
+                    filterSide={'left'}
+                    title={'Unid. Exec.'}
+                  />
+                </div>
               </TableCell>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </TableContainer>
-  </div>
-);
+          </TableHead>
+          <TableBody>
+            {proposals.map((item) => (
+              <TableRow key={item.id} className={'uppercase'}>
+                <TableCell align={'left'} component={'th'} scope={'row'}>
+                  {item.step}
+                </TableCell>
+                <TableCell align={'left'}>{item.proposal}</TableCell>
+                <TableCell align={'left'}>{item.name}</TableCell>
+                <TableCell align={'left'}>{item.techProduct}</TableCell>
+                <TableCell align={'left'}>{item.cnpj}</TableCell>
+                {userType === 'collaborator' ? (
+                  <TableCell align={'left'}>
+                    <div className={'flex flex-col gap-2'}>
+                      <Chip
+                        label={'Reg. de conclusão'}
+                        size={'small'}
+                        variant={item.status.conclusionRecord === true ? 'filled' : 'outlined'}
+                      />
+                      <Chip
+                        label={'SGSET'}
+                        size={'small'}
+                        variant={item.status.sgset === true ? 'filled' : 'outlined'}
+                      />
+                      {item.status.diagnostic ? (
+                        <Chip
+                          label={'Diagnóstico'}
+                          size={'small'}
+                          variant={item.status.diagnostic === true ? 'filled' : 'outlined'}
+                        />
+                      ) : null}
+                    </div>
+                  </TableCell>
+                ) : null}
+                <TableCell align={'left'}>
+                  {userType === 'collaborator'
+                    ? item.relationshipUnity.cfp
+                    : item.relationshipUnity.name}
+                </TableCell>
+                <TableCell align={'left'}>
+                  {userType === 'collaborator' ? item.executingUnity.cfp : item.executingUnity.name}
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </TableContainer>
+    </div>
+  );
+};
